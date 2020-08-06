ec6f8fdce4d1818764faca529594a479ffb9f293
William Banks <williamjb555@gmail.com>
2020-08-06 16:53:04 +0100

    started implementing evaluation

--- src/handlers/EvaluateHandler.cpp
+++ src/handlers/EvaluateHandler.cpp
@@ -41,45 +41,84 @@ double EvaluateHandler::evaluate(std::vector<Element>& elems, std::stringstream*
 		}
 	}
 
-	// TODO decide on an approach
 	// Now how am I actually going to approach this?
 	// Maybe I can get a list of all the brackets, and evaluate them that way? Or each iteration, get the innermost bracket pair?
 	// Or I could split it into vector of snippets, that I can then evaluate one by one?
 
+	// TODO: Implement approach
 	// Current approach decided:
 	// Get a list of all brackets
 	// Loop, evaluating the innermost pair each iteration
-	// When there are no brackets left, evaluate the whole expression
+	// When there are no brackets left, evaluate the whole expression the same as the contents of the brackets
 
-	// A list of all bracket pairs will be useful, depending on my decided approach
+	// A list of all bracket pairs will be useful
 	std::vector<BracketInfo> brackets;
 	int bracketDepth = 0;
 	for(int i = 0; i < elems.size(); i++) {
-		if(i > 0) {
-			Element& prev = elems.at(i - 1);
-			Element& curr = elems.at(i);
+		Element& curr = elems.at(i);
 
-			if(curr.isOpenBracket()) {
-				bracketDepth++;
-				bool function = prev.type == FUNCTION;
-				BracketInfo currBracket = BracketInfo(i, function, bracketDepth);
-				currBracket.closePos = findCloseBracket(elems, i);
+		if(curr.isOpenBracket()) {
+			bracketDepth++;
+			bool function = i > 0 ? elems.at(i - 1).type == FUNCTION : false;
+			BracketInfo currBracket = BracketInfo(i, function, bracketDepth);
+			currBracket.closePos = findCloseBracket(elems, i);
+			if(currBracket.closePos != -1) {
 				brackets.push_back(currBracket);
 			}
 		}
 	}
 
+	std::cout << "Brackets Len: " << brackets.size() << std::endl;
+
 	// Enter main loop for evaluating. Maybe.
-// 	while(elems.size() > 1) {
+	int maxIterations = 1000;
+	int iteration = 0;
+	while(elems.size() > 1) {
 		// Then evaluate any factorials
-		for(int i = 0; i < elems.size(); i++) {
-			if(elems.at(i).isOperator('!') && !elems.at(i - 1).isCloseBracket()) { // Check it's not a '(...)!' situation
-				short rem = 1;
-				applyOperator(elems, i, rem);
-				continue;
+// 		for(int i = 0; i < elems.size(); i++) {
+// 			if(elems.at(i).isOperator('!') && !elems.at(i - 1).isCloseBracket()) { // Check it's not a '(...)!' situation
+// 				short rem = 1;
+// 				applyOperator(elems, i, rem);
+// 				continue;
+// 			}
+// 		}
+
+		// We need the innermost bracket pair, so that there won't be any brackets inside them
+		BracketInfo* innermost = nullptr;
+		int innermostIndex = -1;
+		for(int i = 0; i < brackets.size(); i++) {
+			bool isMoreInner = false;
+			if(!innermost) {
+				isMoreInner = true;
+			} else if(brackets.at(i).depth > innermost->depth) {
+				isMoreInner = true;
+			}
+			if(isMoreInner) {
+				innermost = &brackets.at(i);
+				innermostIndex = i;
 			}
 		}
-// 	}
+
+		// If an innermost bracket has been found
+		if(innermost) {
+			if(!innermost->hasFunction) { // FIXME: Elements are not removed/replaced correctly after evaluating a snippet
+				evalSnippet(elems, innermost->openPos, innermost->closePos); // FIXME: Problem with factorials
+				elems[innermost->openPos] = elems.at(innermost->openPos + 1);
+				elems.erase(elems.begin() + innermost->openPos + 1);
+				std::cout << helpers::Helper::elemsToStr(elems) << std::endl;
+				brackets.erase(brackets.begin() + innermostIndex);
+			} else {
+				// TODO: Handle functions with arguments
+			}
+		}
+
+		// Break out of loop if we've looped more than 1000 times - if we've not got the answer by now we probably won't in the future
+		iteration++;
+		if(iteration > maxIterations) {
+			std::cout << "Could not calculate answer" << std::endl;
+			break;
+		}
+	}
 
 	return 0;
 }
@@ -132,9 +171,10 @@ void EvaluateHandler::applyOperator(std::vector<Element>& elems, int opIndex, sh
 	}
 }
 
-int handlers::EvaluateHandler::findCloseBracket(std::vector<Element>& elems, int afterIndex) {
+int EvaluateHandler::findCloseBracket(std::vector<Element>& elems, int afterIndex) {
 	int depth = 0;
-	for(int i = afterIndex; i < elems.size(); i++) {
+	// afterIndex + 1 so we don't start directly at the opening bracket
+	for(int i = afterIndex + 1; i < elems.size(); i++) {
 		if(elems.at(i).isOpenBracket()) {
 			depth++;
 		} else if(elems.at(i).isCloseBracket()) {
@@ -144,4 +184,17 @@ int handlers::EvaluateHandler::findCloseBracket(std::vector<Element>& elems, int
 		}
 	}
 	return -1;
+}
+
+void EvaluateHandler::evalSnippet(std::vector<Element>& elems, int start, int end) {
+	std::cout << "len: " << elems.size() << " start: " << start << " end: " << end << std::endl;
+	for(int i = start; i < end; i++) {
+		Element& curr = elems.at(i);
+
+		if(curr.type == OPERATOR) {
+			short numRem = 0;
+			applyOperator(elems, i, numRem);
+			i -= numRem;
+		}
+	}
 }
